531a8da1df73845f5d3a33103d9af3f05da318bd
mulbom <87961148+mulbom@users.noreply.github.com>
2023-12-18 15:08:14 +0900

    Create thumbnail.py

---
+++ KB_TeamProject/PYTHON/thumbnail.py
@@ -0,0 +1,218 @@
+import random
+
+class THUMBNAIL:
+    def __init__(self):
+        return None
+
+    def indiJoy(self, rn):
+        indiJoy = [
+            'https://image.bugsm.co.kr/album/images/500/200888/20088894.jpg',
+            'https://image.bugsm.co.kr/album/images/500/203321/20332177.jpg',
+            'https://image.bugsm.co.kr/album/images/500/203030/20303087.jpg',
+            'https://image.bugsm.co.kr/album/images/500/5371/537150.jpg',
+            'https://mblogthumb-phinf.pstatic.net/MjAyMDA4MjBfMTI5/MDAxNTk3OTMzNzI0MTQz.O1YJ0z6M0Sozdku2GX6tGLRQG87rJKMzkj26AfMptacg.sG7361v4KUM6sAqDfl9F8ZMqqDRPBUcPbHPDsVvD1CAg.PNG.yjpiano121/1.PNG?type=w800'
+        ]
+        return indiJoy[rn]
+
+    def indiSad(self, rn):
+        indiSad = [
+            'https://image.bugsm.co.kr/album/images/200/5371/537150.jpg?version=20190223094417.0',
+            'https://image.bugsm.co.kr/album/images/500/204407/20440788.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuSfBZG2MVIdYc_9m-zj0k5GkGYlqVqaECBKJN2fJxpi1YSZ-mGZoA3i76IRa6TWiY37k&usqp=CAU',
+            'https://image.bugsm.co.kr/album/images/500/4345/434551.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1sYejPZHlgZS9IforBpbEKeKP-pj16lYcNY70t9YCQ1UJfOm4mvcc7zecdxHj-X7KVQA&usqp=CAU'
+        ]
+        return indiSad[rn]
+
+    def indiAnger(self, rn):
+        indiAnger = [
+            'https://image.bugsm.co.kr/album/images/500/204374/20437499.jpg',
+            'https://i.namu.wiki/i/Oha6AUfnlwqD8KqZRaIphY6JRuZI2Tma0tO-VgNroc79oyzWCF0ay9H_OlzgqEy5zRZ6YZaJMqUOO4K0heMXaA.webp',
+            'https://image.bugsm.co.kr/album/images/500/202993/20299329.jpg',
+            'https://image.bugsm.co.kr/album/images/500/203408/20340832.jpg',
+            'https://image.bugsm.co.kr/album/images/500/200667/20066727.jpg'
+        ]
+        return indiAnger[rn]
+
+    def indiUnrest(self, rn):
+        indiUnrest = [
+            'https://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/082/707/502/82707502_1653469736017_1_600x600.JPG',
+            'https://image.bugsm.co.kr/album/images/500/201148/20114860.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG2jSXhSw3kqgXse9eeUpHS7jxLAyWuZEYuj0gJad4zfwKqh1l_L7zND4WIAM8uxRelG4&usqp=CAU',
+            'https://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/082/707/502/82707502_1653469736017_1_600x600.JPG',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkUG9nbreEhWyD0533j8C9JBMRIc1eiB-h9TnOUtMfEepc73pUI9o4MjqHbunQjZlHUZE&usqp=CAU'
+        ]
+        return indiUnrest[rn]
+
+    def indiBoring(self, rn):
+        indiBoring = [
+            'https://image.bugsm.co.kr/album/images/500/203794/20379418.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRy-FBRifpWBIRqu6osorxP2R8sUpxNRFt_50aES1NnwYG4lMi3ng0LrGtFGxRAVyl_Cc&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG2jSXhSw3kqgXse9eeUpHS7jxLAyWuZEYuj0gJad4zfwKqh1l_L7zND4WIAM8uxRelG4&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa8JYqDXk25czOVzCAB3341N4bXPkQrwBFU817DuJHWw6NQDKXiPDMOqwdcOlQq3Q8x9s&usqp=CAU',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT23LhR4VTQXEPimUhwXFmXlTisWoCUm6-o4esvj-s1IcO5w7zQD5eRrREdJR11HKlap78&usqp=CAU'
+        ]
+        return indiBoring[rn]
+
+        # 힙합
+
+    def hipJoy(self, rn):
+        hipJoy = [
+            'https://image.bugsm.co.kr/album/images/500/201345/20134575.jpg',
+            'https://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/081/115/600/81115600_1539935579833_1_600x600.JPG',
+            'https://image.bugsm.co.kr/album/images/500/40814/4081408.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0sUhIt1n1Cy5O_X6p1CjfYmH50PStRHoCLt7MKkYa3y22eR2AAp4rGyCIGxUAY4B3e3w&usqp=CAU',
+            'https://image.bugsm.co.kr/album/images/500/201928/20192807.jpg'
+        ]
+        return hipJoy[rn]
+
+    def hipSad(self, rn):
+        hipSad = [
+            'https://image.bugsm.co.kr/album/images/500/203385/20338584.jpg',
+            'https://image.bugsm.co.kr/album/images/500/40397/4039704.jpg',
+            'https://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/080/274/445/80274445_1321517829225_1_600x600.JPG',
+            'https://image.bugsm.co.kr/album/images/350/203682/20368283.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCZUjEd7S4GV5QK9gXyq5zSKJEEcrzBJwr60-tHwFU_0KG2KbmF0lnno1x67st9Roks6I&usqp=CAU'
+        ]
+        return hipSad[rn]
+
+    def hipAnger(self, rn):
+        hipAnger = [
+            'https://i.ytimg.com/vi/iG1wG86hywk/maxresdefault.jpg',
+            'https://images.genius.com/d85fc249587db1eef7c7d6c094b2e2ae.350x500x1.jpg',
+            'https://image.bugsm.co.kr/album/images/500/202669/20266998.jpg',
+            'https://image.bugsm.co.kr/album/images/500/202021/20202166.jpg',
+            'https://image.bugsm.co.kr/album/images/500/40762/4076236.jpg'
+        ]
+        return hipAnger[rn]
+
+    def hipUnrest(self, rn):
+        hipUnrest = [
+            'https://image.bugsm.co.kr/album/images/500/202401/20240116.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb4e8gYAjNjgAhCakNYSXg_scHiNzsgsSagv9Oi6mkikRcu-k2WhKd1YLg9TYu2mNbEG8&usqp=CAU',
+            'https://image.bugsm.co.kr/album/images/500/2782/278213.jpg',
+            'https://image.bugsm.co.kr/album/images/500/202917/20291742.jpg',
+            'https://image.bugsm.co.kr/album/images/500/202817/20281781.jpg'
+        ]
+        return hipUnrest[rn]
+
+    def hipBoring(self, rn):
+        hipBoring = [
+            'https://image.bugsm.co.kr/album/images/500/201303/20130350.jpg',
+            'https://image.bugsm.co.kr/album/images/500/201928/20192846.jpg',
+            'https://image.bugsm.co.kr/album/images/500/202692/20269290.jpg',
+            'https://image.bugsm.co.kr/album/images/500/40735/4073552.jpg',
+            'https://image.bugsm.co.kr/album/images/500/201447/20144701.jpg'
+        ]
+        return hipBoring[rn]
+
+        # 아이돌
+
+    def idolJoy(self, rn):
+        idolJoy = [
+            'https://spnimage.edaily.co.kr/images/Photo/files/NP/S/2015/09/PS15093000133.jpg',
+            'https://image.bugsm.co.kr/album/images/500/141168/14116820.jpg',
+            'https://i.ytimg.com/vi/M7SufjzwS5Q/maxresdefault.jpg',
+            'https://image.bugsm.co.kr/album/images/500/204570/20457046.jpg',
+            'https://image.bugsm.co.kr/album/images/500/201932/20193267.jpg'
+        ]
+        return idolJoy[rn]
+
+    def idolSad(self, rn):
+        idolSad = [
+            'https://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/084/424/563/84424563_1698383295379_1_600x600.JPG',
+            'https://image.bugsm.co.kr/album/images/500/201587/20158703.jpg',
+            'https://image.bugsm.co.kr/album/images/500/202752/20275298.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYASOFmh3Weh29IozPTBC5uph1ZrFz_bD8LB3HYS4fagICHI3eD5owaZhgt4cMd4Mu2O0&usqp=CAU',
+            'https://as2.ftcdn.net/v2/jpg/01/90/90/47/1000_F_190904737_4xdcQAMUDkDUUPP4NIfDYvbGFdqw9Fh4.jpg'
+        ]
+        return idolSad[rn]
+
+    def idolAnger(self, rn):
+        idolAnger = [
+            'https://image-cdn.hypb.st/https%3A%2F%2Fkr.hypebeast.com%2Ffiles%2F2023%2F07%2Fwatch-newjeans-exciting-music-video-for-new-single-eta-ft.jpg?w=960&cbr=1&q=90&fit=max',
+            'https://image.bugsm.co.kr/album/images/500/40724/4072414.jpg',
+            'https://image.news1.kr/system/photos/2014/9/1/997392/article.jpg/dims/optimize',
+            'https://image.bugsm.co.kr/album/images/500/202294/20229446.jpg',
+            'https://image.bugsm.co.kr/album/images/500/201146/20114619.jpg'
+        ]
+        return idolAnger[rn]
+
+    def idolUnrest(self, rn):
+        idolUnrest = [
+            'https://i.ytimg.com/vi/Y4JQokTmBTA/hq720.jpg?sqp=-oaymwEXCK4FEIIDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLANowbtf6UP76eVbiYuERqRQsRavg',
+            'https://image.bugsm.co.kr/album/images/500/40383/4038340.jpg',
+            'https://image.bugsm.co.kr/album/images/500/203890/20389048.jpg',
+            'https://image.bugsm.co.kr/album/images/500/204456/20445654.jpg',
+            'https://i.namu.wiki/i/pAREffOu5MPNPXIQPTIDchPeh1u3Q6B9f0tlG_r7g-sfFItzti0TJUWREitUrblcswoPj0bOUvfU1VmGAOoUYA.webp'
+        ]
+        return idolUnrest[rn]
+
+    def idolBoring(self, rn):
+        idolBoring = [
+            'https://i.namu.wiki/i/THwo-HPVAh9Pbu_pb-axW6r2LgO32VwbsF3tth4Ya8-hSqr6ON6vbeNoKHAH0YhmUNwJh6187q3OpcRtTQY9fg.webp',
+            'https://www.weiv.co.kr/wp-content/uploads/2014/05/20120228140954_0.jpg',
+            'https://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/081/017/733/81017733_1514963221459_1_600x600.JPG',
+            'https://image.bugsm.co.kr/album/images/200/204073/20407398.jpg?version=20230601002041.0',
+            'https://image.bugsm.co.kr/album/images/500/40924/4092452.jpg',
+            'https://image.bugsm.co.kr/album/images/500/5122/512291.jpg',
+            'https://img.sbs.co.kr/newsnet/etv/upload/2015/06/30/30000485441_1280.jpg'
+        ]
+        return idolBoring[rn]
+
+        # 발라드
+
+    def balJoy(self, rn):
+        balJoy = [
+            'https://image.bugsm.co.kr/album/images/500/4328/432855.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG2jSXhSw3kqgXse9eeUpHS7jxLAyWuZEYuj0gJad4zfwKqh1l_L7zND4WIAM8uxRelG4&usqp=CAU',
+            'https://image.bugsm.co.kr/album/images/500/3640/364008.jpg',
+            'https://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/080/991/775/80991775_1506667592967_1_600x600.JPG',
+            'https://is1-ssl.mzstatic.com/image/thumb/Music122/v4/db/42/c9/db42c96c-9bfa-e14d-b678-b03330f7a55f/1_re.jpg/600x600bf-60.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNqJxXNpQbsFgkbEIJK6QNCG1o2mKg3MK1jC-5yTm6bSnGcEVdhC6EPga6owEI8sFIAHc&usqp=CAU'
+        ]
+        return balJoy[rn]
+
+    def balSad(self, rn):
+        balSad = [
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM-P4taKpRg6bWJODIKWFzwi6o8klUucNDEINdkd8-Nt9_aWOIYdWOVRgghq74CEnd8ck&usqp=CAU',
+            'https://image.bugsm.co.kr/album/images/500/9589/958964.jpg',
+            'https://image.bugsm.co.kr/album/images/500/200761/20076172.jpg',
+            'https://cdn.musicow.com/song/210105/736ecdde.jpg',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2DbnfQXKaN_o6RVa0D1MBWNOM8wwlzmKjKh73oKclXiMsyNc6b3SAKlZiKceFTf7JtDg&usqp=CAU',
+            'https://image.bugsm.co.kr/album/images/500/200668/20066833.jpg'
+        ]
+        return balSad[rn]
+
+    def balAnger(self, rn):
+        balAnger = [
+            'https://is1-ssl.mzstatic.com/image/thumb/Music123/v4/c8/4c/c1/c84cc177-18e4-5b81-9762-60442a533c7d/7330178045248.png/1200x1200bf-60.jpg',
+            'https://image.bugsm.co.kr/album/images/500/200387/20038792.jpg',
+            'https://image.bugsm.co.kr/album/images/200/2195/219595.jpg?version=20211216020419.0',
+            'https://t1.daumcdn.net/cfile/tistory/167F3C45507829DD1C',
+            'https://image.bugsm.co.kr/album/images/500/202635/20263533.jpg',
+            'https://image.bugsm.co.kr/album/images/200/40935/4093522.jpg?version=20231207105021.0'
+        ]
+        return balAnger[rn]
+
+    def balUnrest(self, rn):
+        balUnrest = [
+            'https://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/080/338/613/80338613_1357868204463_1_600x600.JPG/dims/resize/Q_80,0',
+            'https://image.bugsm.co.kr/album/images/500/5277/527746.jpg',
+            'https://image.bugsm.co.kr/album/images/500/3440/344041.jpg',
+            'https://image.bugsm.co.kr/album/images/500/80070/8007002.jpg',
+            'https://image.bugsm.co.kr/album/images/500/202415/20241516.jpg',
+            'https://image.bugsm.co.kr/album/images/500/200249/20024926.jpg'
+        ]
+        return balUnrest[rn]
+
+    def balBoring(self, rn):
+        balBoring = [
+            'https://i.ytimg.com/vi/OPo5-e_XacE/maxresdefault.jpg',
+            'https://image.bugsm.co.kr/album/images/500/203875/20387538.jpg',
+            'https://image.bugsm.co.kr/album/images/500/201118/20111878.jpg',
+            'https://image.bugsm.co.kr/album/images/500/203366/20336688.jpg',
+            'https://image.bugsm.co.kr/album/images/500/6248/624831.jpg',
+            'https://image.bugsm.co.kr/album/images/500/200789/20078997.jpg'
+        ]
+        return balBoring[rn]
