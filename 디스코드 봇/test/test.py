70bb250e1b4322ec78f87ce812be5bcb9ac30001
mulbom <87961148+mulbom@users.noreply.github.com>
2024-03-14 20:25:37 +0900

    Update test.py

--- 디스코드 봇/test/test.py
+++ 디스코드 봇/test/test.py
@@ -30,6 +30,43 @@ from discord.ext import commands
         else:
             await ctx.send("이미 밖이에요")
 
+@commands.command()
+    async def play(self, ctx, *, url):
+        """(사전 다운로드하지 않음)"""
+        try:
+            async with ctx.typing():
+                player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+                ctx.voice_client.play(player, after=lambda e: print(f'Player error: {e}') if e else None)
+                await ctx.send(f'현재 재생중: {player.title}')  # 이 줄을 try 블록 안으로 옮겼습니다
+        except Exception as e:
+            print("에러 : ", e)
+            await ctx.send("음악을 재생하는 중에 오류가 발생했습니다.")
+
+    @commands.command()
+    async def volume(self, ctx, volume: int):
+        if ctx.voice_client is None:
+            return await ctx.send("Not connected to a voice channel.")
+
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send(f"Changed volume to {volume}%")
+
+    @commands.command()
+    async def stop(self, ctx):
+        await ctx.voice_client.disconnect()
+
+    @commands.command()
+    async def pause(self, ctx):
+        if ctx.voice_client.is_paused() or not ctx.voice_client.is_playing():
+            await ctx.send("음악이 이미 일시 정지 중이거나 재생 중이지 않습니다.")
+
+        ctx.voice_client.pause()
+
+    @commands.command()
+    async def resume(self, ctx):
+        if ctx.voice_client.is_playing() or not ctx.voice_client.is_paused():
+            await ctx.send("음악이 이미 재생 중이거나 재생할 음악이 존재하지 않습니다.")
+
+        ctx.voice_client.resume()
 
 @bot.event
 async def on_ready():
